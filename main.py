6e69ca421b2055f18a71e4e5790ab6d6f40c33d0
Glevob <mistkatp@yandex.ru>
2025-10-20 11:12:35 +0300

    Initial commit3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import psycopg2
+import psycopg
 import os
 from urllib.parse import urlparse
 
@@ -9,8 +9,8 @@ app = Flask(__name__)
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     url = urlparse(DATABASE_URL)
-    conn = psycopg2.connect(
-        database=url.path[1:],
+    conn = psycopg.connect(
+        dbname=url.path[1:],
         user=url.username,
         password=url.password,
         host=url.hostname,
