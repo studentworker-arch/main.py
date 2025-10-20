4f25326d802449f9e951c028ac97cd2ac9be59d1
Glevob <mistkatp@yandex.ru>
2025-10-20 10:09:51 +0300

    Initial commit3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Serverless! 🚀\n", 300, {'Content-Type': 'text/plain'}
+    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
 
 @app.route('/echo', methods=['POST'])
 def echo():
@@ -14,3 +14,6 @@ def echo():
         "you_sent": data,
         "length": len(str(data)) if data else 0
     })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
